743c2581a2a3c54ce4977a7fce50c26e6888ea4d
Aaron Lal <lucidlear@gmail.com>
2024-03-01 09:44:50 +1100

    output

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I like pizza");
-            Console.WriteLine("Its really good");
-            Console.Beep();
+            Console.Write("Hey!");
+            Console.WriteLine("Hello!");
+
+            // This is a comment
+            /*
+             * this
+             * is a
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("Aaron\nLal");
+
+            Console.ReadKey();
         }
     }
 }
