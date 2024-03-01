e9bb6276ec036e6ca9265911496292f38ea1b185
Aaron Lal <lucidlear@gmail.com>
2024-03-01 13:11:39 +1100

    type casting

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-            const double pi = 3.14159;
+            double a = 3.14;
 
-            Console.WriteLine(pi);
+            int b = Convert.ToInt32(a);
 
+            int c = 123;
+            double d = Convert.ToDouble(c);
+
+            int e = 321;
+            string f = Convert.ToString(e);
+
+            string g = "$";
+            char h = Convert.ToChar(g);
+
+            string i = "true";
+            bool j = Convert.ToBoolean(i);
+            
+
+
+
+            Console.WriteLine(b.GetType);
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(f.GetType());
+            Console.WriteLine(h.GetType());
+            Console.WriteLine(j.GetType());
 
         }
     }
