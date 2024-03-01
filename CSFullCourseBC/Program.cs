3bb39b4fcc5bb6faded23aa69847c26bc98d1e63
Aaron Lal <lucidlear@gmail.com>
2024-03-01 11:12:20 +1100

    constants

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,29 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int x; // declaration
-            x = 123; // initialization
+            const double pi = 3.14159;
 
-            int y = 321; // declaration + initization
-
-            int z = x + y;
-
-            int age = 21; // whole integer
-            double height = 300.5; // decimal number
-            bool alive = false;
-            char symbol = '@';
-            string name = "Aaron";
+            Console.WriteLine(pi);
 
 
-            Console.WriteLine("Hello " + name);
-            Console.WriteLine("your age is: " + age);
-            Console.WriteLine("You height is: " + height + "cm");
-            Console.WriteLine("Are you alive? " + alive);
-            Console.WriteLine("You symbol is " + symbol);
-
-            string userName = symbol + name;
-
-            Console.WriteLine("Your username is: " + userName);
         }
     }
 }
