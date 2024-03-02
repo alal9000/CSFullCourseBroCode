32109f198fd63403a02b36c410d698b44e3e081d
Aaron Lal <lucidlear@gmail.com>
2024-03-03 09:55:22 +1100

    conditional operator

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,41 +4,11 @@
     {
         static void Main(string[] args)
         {
+            double temperature = 20;
+            string message;
 
-            double x;
-            double y;
-            double result;
+            Console.WriteLine((temperature >= 15) ? "Its warm outside" : "Its cold outside");
 
-            try
-            {
-
-                Console.Write("Enter number 1: ");
-                x = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Enter number 2: ");
-                y = Convert.ToInt32(Console.ReadLine());
-
-                result = x / y;
-
-                Console.WriteLine("Result: " + result);
-
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine("You can't divide by zero");
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine("Enter only numbers please");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("something went wrong");
-            }
-            finally
-            {
-                Console.WriteLine("Thanks for visiting");
-            }
 
         }
 
