882ee52dc1f4c37426abfbabe9f4eb1fdf1831d6
Aaron Lal <lucidlear@gmail.com>
2024-03-03 09:46:56 +1100

    exception handling

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -5,24 +5,44 @@
         static void Main(string[] args)
         {
 
-            double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+            double x;
+            double y;
+            double result;
 
-            Console.WriteLine(total);
-
-
-        }
-
-        static double CheckOut(params double[] prices)
-        {
-            double total = 0;
-
-            foreach (double price in prices)
+            try
             {
-                total += price;
+
+                Console.Write("Enter number 1: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                result = x / y;
+
+                Console.WriteLine("Result: " + result);
+
             }
-            
-            return total;
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You can't divide by zero");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter only numbers please");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("something went wrong");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for visiting");
+            }
+
         }
+
+      
 
     }
 }
