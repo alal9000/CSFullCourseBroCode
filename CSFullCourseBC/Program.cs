b6bf87aa671d430d17b52f0ffe3c5f8dc5a669ae
Aaron Lal <lucidlear@gmail.com>
2024-03-03 16:41:12 +1100

    constructors

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,22 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Human human1 = new Human();
-            Human human2 = new Human();
+            //Human human1 = new Human("Rick", 65);
+            //Human human2 = new Human("Morty", 16);
 
-            human1.name = "Rick";
-            human1.age = 65;
+            //human1.Eat();
+            //human1.Sleep();
+            //human1.howOld();
 
-            human1.Eat();
-            human1.Sleep();
-            human1.howOld();
+            //human2.Eat();
+            //human2.Sleep();
+            //human2.howOld();
 
-            human2.name = "Morty";
-            human2.age = 16;
+            Car car1 = new Car("Ford", "Mustang", 2022, "red");
+            Car car2 = new Car("Chevy", "Corvette", 2021, "blue");
 
-            human2.Eat();
-            human2.Sleep();
-            human2.howOld();
+            car1.Drive();
+            car2.Drive();
 
 
 
@@ -27,26 +27,53 @@
         }
 
     }
-    class Human
+    //class Human
+    //{
+    //    public string name;
+    //    public int age;
+
+    //    public Human(string name, int age)
+    //    {
+    //        this.name = name;
+    //        this.age = age;
+    //    }
+
+    //    public void Eat()
+    //    {
+    //        Console.WriteLine(name + " is eating");
+    //    }
+
+    //    public void Sleep()
+    //    {
+    //        Console.WriteLine(name + " is sleeping");
+    //    }
+
+    //    public void howOld()
+    //    {
+    //        Console.WriteLine(name + " is " + age + " years old");
+    //    }
+
+    //}
+
+    class Car
     {
-        public string name;
-        public int age;
+        string make;
+        string model;
+        int year;
+        string color;
 
-        public void Eat()
+        public Car(string make, string model, int year, string color)
         {
-            Console.WriteLine(name + " is eating");
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.color = color;
         }
 
-        public void Sleep()
+        public void Drive()
         {
-            Console.WriteLine(name + " is sleeping");
+            Console.WriteLine("You drive the " + make + " " + model);
         }
-
-        public void howOld()
-        {
-            Console.WriteLine(name + " is " + age + " years old");
-        }
-
     }
 
       
