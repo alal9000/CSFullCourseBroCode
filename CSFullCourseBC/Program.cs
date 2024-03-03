e39815174f1b2cb00c35210d9bd494ffea8ba3a2
Aaron Lal <lucidlear@gmail.com>
2024-03-03 15:48:45 +1100

    objects

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -4,9 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Messages.Hello();
-            Messages.Waiting();
-            Messages.Bye();
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.name = "Rick";
+            human1.age = 65;
+
+            human1.Eat();
+            human1.Sleep();
+            human1.howOld();
+
+            human2.name = "Morty";
+            human2.age = 16;
+
+            human2.Eat();
+            human2.Sleep();
+            human2.howOld();
+
+
+
+
+        }
+
+    }
+    class Human
+    {
+        public string name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+
+        public void howOld()
+        {
+            Console.WriteLine(name + " is " + age + " years old");
         }
 
     }
