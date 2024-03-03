b538d2ca13ff794cbf9a6ed7c7b2e401c248a3ed
Aaron Lal <lucidlear@gmail.com>
2024-03-03 13:34:23 +1100

    classes

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -1,39 +1,17 @@
 ﻿namespace CSFullCourseBC
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            //string[] ford = { "mustang", "f-150", "explorer" };
-            //string[] chevy = { "corvette", "camero", "silverado" };
-            //string[] toyota = { "corolla", "camry", "rav4" };
-
-            string[,] parkingLot = { { "mustang", "f-150", "explorer" }, 
-                                     { "corvette", "camero", "silverado" }, 
-                                     { "corolla", "camry", "rav4" } 
-                                   };
-
-            parkingLot[0, 2] = "Fusion";
-            parkingLot[2, 0] = "Tacoma";
-
-            //foreach (string car in parkingLot)
-            //{
-            //    Console.WriteLine(car);
-            //}
-
-            for (int i = 0; i < parkingLot.GetLength(0); i++)
-            {
-                for (int j = 0; j < parkingLot.GetLength(1); j++)
-                {
-                    Console.Write(parkingLot[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-
-
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
         }
+
+    }
 
       
 
-    }
+    
 }
