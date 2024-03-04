9e68afc819ab2d4a23886b04316babf43e9be0bc
Aaron Lal <lucidlear@gmail.com>
2024-03-04 18:41:50 +1100

    abstract classes

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -8,23 +8,13 @@
             Bicycle bicycle = new Bicycle();
             Boat boat = new Boat();
 
-            Console.WriteLine(car.speed);
-            Console.WriteLine(car.wheels);
-            car.Go();
+            Vehicle vehicle = new Vehicle();
 
-
-            Console.WriteLine(bicycle.speed);
-            Console.WriteLine(bicycle.wheels);
-            bicycle.Go();
-
-            Console.WriteLine(boat.speed);
-            Console.WriteLine(boat.wheels);
-            boat.Go();
 
         }
 
     }
-    class Vehicle
+    abstract class Vehicle
     {
         public int speed = 0;
 
@@ -37,16 +27,19 @@
     class Car : Vehicle
     {
         public int wheels = 4;
+        int maxSpeed = 500;
     }
 
     class Bicycle : Vehicle
     {
         public int wheels = 2;
+        int maxSpeed = 50;
     }
 
     class Boat : Vehicle
     {
         public int wheels = 0;
+        int maxSpeed = 100;
     }
 
 
