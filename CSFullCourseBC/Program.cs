3bb3d31328a53238d661076511a492b8445f12e5
Aaron Lal <lucidlear@gmail.com>
2024-03-06 07:49:06 +1100

    generics

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -5,50 +5,57 @@ namespace CSFullCourseBC
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(Planets.Mercury + " is planet #" + (int)Planets.Mercury);
-            //Console.WriteLine(Planets.Pluto + " is planet #" + (int)Planets.Pluto);
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArray = { 1.0, 2.0, 3.0 };
+            string[] stringArray = { "1", "2", "3" };
 
-            string name = PlanetRadius.Earth.ToString();
-            int radius = (int)PlanetRadius.Earth;
-            double volume = Volume(PlanetRadius.Earth);
+            DisplayElements(intArray);
+            DisplayElements(doubleArray);
+            DisplayElements(stringArray);
 
-            Console.WriteLine("planet: " + name);
-            Console.WriteLine("radius: " + radius + "km");
-            Console.WriteLine("volume: " + volume + "km^3");
 
         }
-        public static double Volume(PlanetRadius radius)
+        //public static void DisplayElements(int[] array)
+        //{
+        //    foreach (int item in array)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //}
+
+        //public static void DisplayElements(double[] array)
+        //{
+        //    foreach (double item in array)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //}
+
+        //public static void DisplayElements(string[] array)
+        //{
+        //    foreach (string item in array)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+        //    Console.WriteLine();
+
+        //}
+
+        public static void DisplayElements<Thing>(Thing[] array)
         {
-            double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+            foreach (Thing item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
 
-            return volume;
         }
 
-    }
-    public enum Planets
-    {
-        Mercury = 1,
-        Venus = 2,
-        Earth = 3,
-        Mars = 4,
-        Jupiter = 5,
-        Saturn = 6,
-        Uranus = 7,
-        Neptune = 8,
-        Pluto = 9
-    }
 
-    public enum PlanetRadius
-    {
-        Mercury = 2439,
-        Venus = 6051,
-        Earth = 6371,
-        Mars = 3389,
-        Jupiter = 69911,
-        Saturn = 58232,
-        Uranus = 25362,
-        Neptune = 24622,
-        Pluto = 1188
     }
 
 
