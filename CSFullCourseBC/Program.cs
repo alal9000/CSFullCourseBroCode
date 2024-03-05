c6b485ddc93a3e13ab56f5ad203fea94315fe151
Aaron Lal <lucidlear@gmail.com>
2024-03-06 06:53:20 +1100

    enums

--- CSFullCourseBC/Program.cs
+++ CSFullCourseBC/Program.cs
@@ -5,31 +5,51 @@ namespace CSFullCourseBC
     {
         static void Main(string[] args)
         {
-            Car car = new Car("Civic");
+            //Console.WriteLine(Planets.Mercury + " is planet #" + (int)Planets.Mercury);
+            //Console.WriteLine(Planets.Pluto + " is planet #" + (int)Planets.Pluto);
 
-            Console.WriteLine(car.Model);
+            string name = PlanetRadius.Earth.ToString();
+            int radius = (int)PlanetRadius.Earth;
+            double volume = Volume(PlanetRadius.Earth);
+
+            Console.WriteLine("planet: " + name);
+            Console.WriteLine("radius: " + radius + "km");
+            Console.WriteLine("volume: " + volume + "km^3");
 
         }
-
-    }
-
-    class Car
-    {
-        //string model;
-
-        //public string Model
-        //{
-        //    get { return model; }
-        //    set { model = value; }
-        //}
-
-        public string Model { get; set; } // this is the same as the above comment
-
-        public Car(string model)
+        public static double Volume(PlanetRadius radius)
         {
-            this.Model = model;
+            double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+
+            return volume;
         }
 
     }
+    public enum Planets
+    {
+        Mercury = 1,
+        Venus = 2,
+        Earth = 3,
+        Mars = 4,
+        Jupiter = 5,
+        Saturn = 6,
+        Uranus = 7,
+        Neptune = 8,
+        Pluto = 9
+    }
+
+    public enum PlanetRadius
+    {
+        Mercury = 2439,
+        Venus = 6051,
+        Earth = 6371,
+        Mars = 3389,
+        Jupiter = 69911,
+        Saturn = 58232,
+        Uranus = 25362,
+        Neptune = 24622,
+        Pluto = 1188
+    }
+
 
 }
